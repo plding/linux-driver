6926beeeca0164c3cd2c5149c87d9373e87b5ccb
plding <77676182@qq.com>
2017-11-01 14:33:16 +0800

    update

--- ch06/globalmem2.c
+++ ch06/globalmem2.c
@@ -16,9 +16,9 @@
 #define GLOBALMEM_SIZE 0x1000
 #define MEM_CLEAR 0x01
 #define GLOBALMEM_MAJOR 230
+#define DEVICE_NUM 10
 
 static int globalmem_major = GLOBALMEM_MAJOR;
-module_param(globalmem_major, int, S_IRUGO);
 
 struct globalmem_dev {
     struct cdev cdev;
@@ -29,31 +29,14 @@ struct globalmem_dev *globalmem_devp;
 
 static int globalmem_open(struct inode *inode, struct file *filp)
 {
-    filp->private_data = globalmem_devp;
+    struct globalmem_dev *dev = container_of(inode->i_cdev,
+                struct globalmem_dev, cdev);
+    filp->private_data = dev;
     return 0;
 }
 
 static int globalmem_release(struct inode *inode, struct file *filp)
 {
-    return 0;
-}
-
-static long globalmem_ioctl(struct file *filp, unsigned int cmd,
-    unsigned long arg)
-{
-    struct globalmem_dev *dev = filp->private_data;
-
-    switch (cmd) {
-
-    case MEM_CLEAR:
-        memset(dev->mem, 0, GLOBALMEM_SIZE);
-        printk(KERN_INFO "globalmem is set to zero");
-        break;
-
-    default:
-        return -EINVAL;
-    }
-
     return 0;
 }
 
@@ -70,9 +53,9 @@ static ssize_t globalmem_read(struct file *filp, char __user *buf, size_t size,
     if (count > GLOBALMEM_SIZE - p)
         count = GLOBALMEM_SIZE - p;
 
-    if (copy_to_user(buf, dev->mem + p, count)) {
+    if (copy_to_user(buf, dev->mem + p, count))
         ret = -EFAULT;
-    } else {
+    else {
         *ppos += count;
         ret = count;
 
@@ -105,6 +88,25 @@ static ssize_t globalmem_write(struct file *filp, const char __user *buf,
     }
 
     return ret;
+}
+
+static long globalmem_ioctl(struct file *filp, unsigned int cmd,
+    unsigned long arg)
+{
+    struct globalmem_dev *dev = filp->private_data;
+
+    switch (cmd) {
+
+    case MEM_CLEAR:
+        memset(dev->mem, 0, GLOBALMEM_SIZE);
+        printk(KERN_INFO "globalmem is set to zero");
+        break;
+
+    default:
+        return -EINVAL;
+    }
+
+    return 0;
 }
 
 static loff_t globalmem_llseek(struct file *filp, loff_t offset, int orig)
@@ -157,10 +159,10 @@ static const struct file_operations globalmem_fops = {
     .release = globalmem_release,
 };
 
-static void globalmem_setup_cdev(struct globalmem_dev *dev, int index)
+static void globalmem_setup_dev(struct globalmem_dev *dev, int index)
 {
     int err, devno = MKDEV(globalmem_major, index);
-
+    
     cdev_init(&dev->cdev, &globalmem_fops);
     dev->cdev.owner = THIS_MODULE;
     err = cdev_add(&dev->cdev, devno, 1);
@@ -170,37 +172,42 @@ static void globalmem_setup_cdev(struct globalmem_dev *dev, int index)
 
 static int __init globalmem_init(void)
 {
-    int ret;
+    int ret, i;
     dev_t devno = MKDEV(globalmem_major, 0);
 
     if (globalmem_major)
-        ret = register_chrdev_region(devno, 1, "globalmem");
+        ret = register_chrdev_region(devno, DEVICE_NUM, "globalmem2");
     else {
-        ret = alloc_chrdev_region(&devno, 0, 1, "globalmem");
+        ret = alloc_chrdev_region(&devno, 0, DEVICE_NUM, "globalmem2");
         globalmem_major = MAJOR(devno);
     }
     if (ret < 0)
         return ret;
 
-    globalmem_devp = kzalloc(sizeof(struct globalmem_dev), GFP_KERNEL);
+    globalmem_devp = kzalloc(sizeof(struct globalmem_dev) * DEVICE_NUM, GFP_KERNEL);
     if (!globalmem_devp) {
         ret = -ENOMEM;
         goto fail_malloc;
     }
 
-    globalmem_setup_cdev(globalmem_devp, 0);
+    for (i = 0; i < DEVICE_NUM; i++)
+        globalmem_setup_dev(globalmem_devp + i, i);
+
     return 0;
 
 fail_malloc:
-    unregister_chrdev_region(devno, 1);
+    unregister_chrdev_region(devno, DEVICE_NUM);
     return ret;
 }
 
 static void __exit globalmem_exit(void)
 {
-    cdev_del(&globalmem_devp->cdev);
+    int i;
+
+    for (i = 0; i < DEVICE_NUM; i++)
+        cdev_del(&globalmem_devp[i].cdev);
     kfree(globalmem_devp);
-    unregister_chrdev_region(MKDEV(globalmem_major, 0), 1);
+    unregister_chrdev_region(MKDEV(globalmem_major, 0), DEVICE_NUM);
 }
 
 MODULE_AUTHOR("Ding Peilong <77676182@qq.com>");
